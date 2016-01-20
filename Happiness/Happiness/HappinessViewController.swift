306feeb92b01f2c46e1e40cfc72ef10babad2a07
Kate Arapova <katearapova@gmail.com>
2016-01-20 17:39:08 +0300

    Lecture 5 Demo

---
+++ Happiness/Happiness/HappinessViewController.swift
@@ -0,0 +1,13 @@
+//
+//  HappinessViewController.swift
+//  Happiness
+//
+//  Created by Kate Arapova on 12.01.16.
+//  Copyright © 2016 Kateryna Arapova. All rights reserved.
+//
+
+import UIKit
+
+class HappinessViewController: UIViewController {
+
+}
